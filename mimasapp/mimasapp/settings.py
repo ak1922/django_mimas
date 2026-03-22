51eb25006dbbcd7ba23c5200f076c5e39836eaec
ak1922 <aoakyaw@protonmail.com>
2026-03-22 12:17:20 -0400

    pos

--- mimasapp/mimasapp/settings.py
+++ mimasapp/mimasapp/settings.py
@@ -14,11 +14,11 @@ import os
 import logging
 from pathlib import Path
 from django.contrib.messages import constants as messages
-
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -89,10 +89,24 @@ WSGI_APPLICATION = 'mimasapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "CONN_MAX_AGE": 60,
+        "OPTIONS": {
+            # "options": "-c statement_timeout=5000"
+        },
     }
 }
 
